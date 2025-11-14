5866cc5b0e51f51df460dac7309344bd97210cd7
Fabrice-TV <a20222297@pucp.edu.pe>
2025-11-13 20:41:16 -0500

    Segundo Commit

---
+++ src/main/java/org/example/cliente_lab10/ClienteLab10Application.java
@@ -0,0 +1,13 @@
+package org.example.cliente_lab10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteLab10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteLab10Application.class, args);
+    }
+
+}
